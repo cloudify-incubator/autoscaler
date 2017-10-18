e9b2dd67574e71cca1e278bc30439b43b8611025
Denis Pauk <pauk.denis@gmail.com>
2017-10-18 13:59:31 +0300

    Split code to several files

--- cluster-autoscaler/cloudprovider/cloudifyprovider/init.go
+++ cluster-autoscaler/cloudprovider/cloudifyprovider/init.go
@@ -22,16 +22,8 @@ import (
 	cloudify "github.com/cloudify-incubator/cloudify-rest-go-client/cloudify"
 	"github.com/golang/glog"
 	"io"
-	apiv1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
-	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
-	"k8s.io/autoscaler/cluster-autoscaler/utils/errors"
 	"os"
 )
-
-type CloudifyScaleProvider struct {
-	client *cloudify.CloudifyClient
-}
 
 type CloudifyProviderConfig struct {
 	Host       string `json:"host,omitempty"`
@@ -39,44 +31,6 @@ type CloudifyProviderConfig struct {
 	Password   string `json:"password,omitempty"`
 	Tenant     string `json:"tenant,omitempty"`
 	Deployment string `json:"deployment,omitempty"`
-}
-
-// Name returns name of the cloud provider.
-func (cl *CloudifyScaleProvider) Name() string {
-	glog.Warning("Name")
-	return "cloudify"
-}
-
-// NodeGroups returns all node groups configured for this cloud provider.
-func (cl *CloudifyScaleProvider) NodeGroups() []cloudprovider.NodeGroup {
-	glog.Warning("NodeGroups")
-	return []cloudprovider.NodeGroup{}
-}
-
-// NodeGroupForNode returns the node group for the given node.
-func (cl *CloudifyScaleProvider) NodeGroupForNode(node *apiv1.Node) (cloudprovider.NodeGroup, error) {
-	glog.Warningf("NodeGroupForNode %+v", node)
-	return nil, cloudprovider.ErrNotImplemented
-}
-
-// Pricing returns pricing model for this cloud provider or error if not available.
-func (cl *CloudifyScaleProvider) Pricing() (cloudprovider.PricingModel, errors.AutoscalerError) {
-	glog.Warning("Pricing")
-	return nil, cloudprovider.ErrNotImplemented
-}
-
-// GetAvailableMachineTypes get all machine types that can be requested from the cloud provider.
-// Implementation optional.
-func (cl *CloudifyScaleProvider) GetAvailableMachineTypes() ([]string, error) {
-	glog.Warning("GetAvailableMachineTypes")
-	return []string{}, cloudprovider.ErrNotImplemented
-}
-
-// NewNodeGroup builds a theoretical node group based on the node definition provided. The node group is not automatically
-// created on the cloud provider side. The node group is not returned by NodeGroups() until it is created.
-func (cl *CloudifyScaleProvider) NewNodeGroup(machineType string, labels map[string]string, extraResources map[string]resource.Quantity) (cloudprovider.NodeGroup, error) {
-	glog.Warningf("NewNodeGroup: %+v %+v %+v", machineType, labels, extraResources)
-	return nil, cloudprovider.ErrNotImplemented
 }
 
 func BuildCloudifyCloud(config io.Reader) (*CloudifyScaleProvider, error) {
